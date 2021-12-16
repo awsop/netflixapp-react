47ec53050ac3e5393c5c8b29b5946f8880b3926b
Nagasatish Vadlamuri <91796133+awsop@users.noreply.github.com>
2021-12-16 22:38:48 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDVooKJ5t_8pCpYJ-ZQDbq4HK2B6JJKV28",
-    authDomain: "netflix-clone-9f83e.firebaseapp.com",
-    projectId: "netflix-clone-9f83e",
-    storageBucket: "netflix-clone-9f83e.appspot.com",
-    messagingSenderId: "339454087158",
-    appId: "1:339454087158:web:63a9c479f14964634b41d7",
-    measurementId: "G-ZEH7WFGFJC"
+  apiKey: "AIzaSyCGdG4lGTIAy1MmTDEkRZZXWmT9nuOoZck",
+  authDomain: "netflix-a2c82.firebaseapp.com",
+  databaseURL: "https://netflix-a2c82-default-rtdb.firebaseio.com",
+  projectId: "netflix-a2c82",
+  storageBucket: "netflix-a2c82.appspot.com",
+  messagingSenderId: "102916231896",
+  appId: "1:102916231896:web:d66c1e0759bfff67433dad"
   };
